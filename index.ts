af424a926f5be6f325855fc366f74073b3fd8fa0
Edgar Dorausch <EdgarDorausch@web.de>
2020-03-29 14:31:22 +0200

    Add repl mode

--- index.ts
+++ index.ts
@@ -1,8 +1,23 @@
 import { toAst } from './visitor';
 import { Tokens, allTokens } from './tokens';
+import * as readline from 'readline';
+
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 
-const str = '1 - 2 * 6';
+rl.question('>>> ', (answer) => {
+  
+  let ast = toAst(answer);
+  console.log(ast.toString())
+  console.log(ast.eval())
 
-let ast = toAst(str);
-console.log(ast.toString())
+  rl.close();
+});
+
+const str = '1 - 2 * 6 + 7';
+
+// let ast = toAst(str);
+// console.log(ast.toString())
