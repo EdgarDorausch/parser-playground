1f0c02263204ac007efa717fcbb92ae225046e67
Edgar Dorausch <EdgarDorausch@web.de>
2020-03-29 15:41:14 +0200

    Added let token

--- tokens.ts
+++ tokens.ts
@@ -1,14 +1,22 @@
 import { createToken, Lexer } from 'chevrotain';
 
-const WhiteSpace =createToken({ name: "WhiteSpace", pattern: /[ \t\n\r]+/, group: Lexer.SKIPPED});
+const WhiteSpace = createToken({ name: "WhiteSpace", pattern: /[ \t\n\r]+/, group: Lexer.SKIPPED});
+
+const Keywords = {
+  LET: 'let',
+  DELETE: 'delete',
+}
+const withoutKeywords = new RegExp(`(?!${Object.values(Keywords).join('|')}\\b)\\b\\w+`)
+console.log(withoutKeywords.source)
 
 export const Tokens = {
+  LetToken:           createToken({ name: "LetToken",           pattern: Keywords.LET}),
   LBracket:           createToken({ name: "LBracket",           pattern: /\(/ }),
   RBracket:           createToken({ name: "RBracket",           pattern: /\)/ }),
   OpName:             createToken({ name: "OpName",             pattern: /(\*|\+|\\|=|-|~|%|!|<|>|\/)+/}),
-  VarName:            createToken({ name: "VarName",            pattern: /[a-z]+/}),
-  // FunctionIdentifier: createToken({ name: "FunctionIdentifier", pattern: /[a-z]+/}),
   NumberLiteral:      createToken({ name: "NumberLiteral",      pattern: /-?(0|[1-9]\d*)(\.\d+)?([eE][+-]?\d+)?/}),
+  VarName:            createToken({ name: "VarName",            pattern: withoutKeywords}),
+  // FunctionIdentifier: createToken({ name: "FunctionIdentifier", pattern: /[a-z]+/}),
 }
 
 export const allTokens = [WhiteSpace, ...Object.values(Tokens)]
